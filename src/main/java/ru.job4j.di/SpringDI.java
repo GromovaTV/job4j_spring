c264789942e020a7407730d7777a77dbcfff3bcd
Tatyana <quininthenorth@gmail.com>
2023-09-26 11:47:50 +0400

    Время жизни (Scope)

--- src/main/java/ru.job4j.di/SpringDI.java
+++ src/main/java/ru.job4j.di/SpringDI.java
@@ -8,8 +8,8 @@ public class SpringDI {
         context.scan("ru.job4j.di");
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
-        ui.print();
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
     }
 }
--- src/main/java/ru.job4j.di/StartUI.java
+++ src/main/java/ru.job4j.di/StartUI.java
@@ -2,20 +2,21 @@ package ru.job4j.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Scope;
 
 @Component
+@Scope("prototype")
 public class StartUI {
+    @Autowired
     private Store store;
-    private ConsoleInput consoleInput;
 
     @Autowired
-    public void setStore(Store store) {
-        this.store = store;
-    }
+    private ConsoleInput input;
 
     public void add(String value) {
         store.add(value);
     }
+
 
     public void print() {
         for (String value : store.getAll()) {
